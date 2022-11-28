e34e3bb3354d65929ece3951e9fa9378a1d7e8d7
Thorerkul2 <119314292+Thorerkul2@users.noreply.github.com>
2022-11-28 21:13:00 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,13 @@ from tkinter import *
 import random
 import time
 
+isInEditor = False
+def loadLevel(filename):
+    global level, levelr, levelname
+    levelname = filename
+    level = open(filename, "a")
+    levelr = open(filename, "r")
+
 class Ball:
     def __init__(self, canvas, color, x, y):
         self.canvas = canvas
@@ -11,6 +18,8 @@ class Ball:
         self.y = 0
         self.canvas_height = self.canvas.winfo_height()
         
+        #self.sprite = canvas.create_image(200, 470, image=PhotoImage(file='/Users/thomasholmesland/Documents/Thor/Programmering/Skoleprosjekt 2022/src/assets/art/ball.png'))
+
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         self.pos = self.canvas.coords(self.id)
@@ -36,19 +45,55 @@ class Racket:
         self.x = 0
         self.y = 0
         self.canvas_height = self.canvas.winfo_height()
+        self.pos = self.canvas.coords(self.id)
         
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         self.pos = self.canvas.coords(self.id)
-        if frame > 60:
+        if frame > 10:
             self.move()
         
     def move(self):
-        if self.pos[1] < mouse_y:
-            self.y = 5
-        if self.pos[1] > mouse_y:
-            self.y = -5
+        if self.pos[1] < mouse_y - 37:
+            self.y = 10
+        if self.pos[1] > mouse_y - 37:
+            self.y = -10
         #print(mouse_y, self.pos[1], frame)
+
+class BallSpawner:
+    def __init__(self):
+        self.levela = level
+        self.isInEditor = isInEditor
+        #print(levelr.read())
+
+        if isInEditor:
+            canvas.bind_all('<KeyPress-Up>', self.writePos)
+
+    def start(self):
+        if isInEditor == False:
+            self.read()
+
+    def update(self):
+        global levelr
+        levelr = open(levelname, "r")
+
+    def read(self):
+        levelt = levelr.read()
+        levelt = levelt.split()
+        
+        global balllist
+        for i, num in enumerate(levelt):
+            ball = Ball(canvas, 'red', (i * 100) + 600, num)
+            balllist.append(ball)
+
+    def write(self):
+        pass
+
+    def writePos(self, evt):
+        pos = racket.pos[1] - 37
+        print(pos)
+
+        level.write(str(int(pos)) + " ")
 
 tk = Tk()
 tk.title("Game")
@@ -71,25 +116,34 @@ tk.bind('<Motion>', motion)
 
 balllist = []
 def makeBalls(lstx, lsty):
-    e = 0
-    for num in lstx:
-        ball = Ball(canvas, 'red', num, 250)
+    for i, num in enumerate(lstx):
+        ball = Ball(canvas, 'red', num, lsty[i])
         balllist.append(ball)
-        e += 1
-    print(e)
 
+def on_closing():
+    level.close()
+    levelr.close()
+    tk.destroy()
+
+tk.protocol("WM_DELETE_WINDOW", on_closing)
+
+loadLevel("level1.txt")
 isDead = False
+ballspawner = BallSpawner()
 
 racket = Racket(canvas, 'blue')
-ball = Ball(canvas, 'red', 500, 250)
+#ball = Ball(canvas, 'red', 500, 250)
 
-makeBalls([600, 600, 600, 600, 600], 
-          [250, 300, 400, 500, 600])
+#makeBalls([600, 700, 800, 900, 1000], 
+#          [300, 250, 200, 150, 100])
 
+
+ballspawner.start()
 while 1:
+    ballspawner.update()
     if isDead == False:
         racket.draw()
-        ball.draw()
+        #ball.draw()
         for ball in balllist:
             ball.draw()
     tk.update_idletasks()
